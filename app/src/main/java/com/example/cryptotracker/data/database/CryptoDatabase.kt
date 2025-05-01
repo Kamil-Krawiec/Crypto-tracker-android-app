0d8d3596d732936eecb911dd149553ef3a7bc698
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2025-05-01 16:37:48 +0200

    first phase building files and making dirs

---
+++ app/src/main/java/com/example/cryptotracker/data/database/CryptoDatabase.kt
@@ -0,0 +1,18 @@
+package com.example.cryptotracker.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.cryptotracker.data.dao.AssetDao
+import com.example.cryptotracker.data.dao.AlertDao
+import com.example.cryptotracker.data.entity.CryptoAsset
+import com.example.cryptotracker.data.entity.PriceAlert
+
+@Database(
+    entities = [CryptoAsset::class, PriceAlert::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class CryptoDatabase : RoomDatabase() {
+    abstract fun assetDao(): AssetDao
+    abstract fun alertDao(): AlertDao
+}
